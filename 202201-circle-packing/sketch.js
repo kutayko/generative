de6247f6064f35538e9d4fa1c04065622560d7c8
Kutay Kocayurt <kkocayurt@babbel.com>
2022-01-08 18:45:59 +0100

    circle packing

---
+++ 202201-circle-packing/sketch.js
@@ -0,0 +1,55 @@
+let sketch = new p5((p) => {
+
+    p.setup = function () {
+        p.createCanvas(600, 600);
+        p.background(0);
+        p.stroke(0);
+
+        maxCircles = 1000000;
+
+        let minDiameter = 5;
+        let maxDiameter = 200;
+        let circles = [];
+
+        let i = 0;
+        while (i < maxCircles) {
+            let d = p.random(minDiameter, maxDiameter);
+            let x = p.random(d / 2, p.width - d / 2);
+            let y = p.random(d / 2, p.height - d / 2);
+
+            let pos = p.createVector(x, y);
+            let circle = new Circle(p, pos, d);
+
+            let noOverlap = circles.every((c) => !Circle.overlaps(circle, c));
+            if (noOverlap) {
+                circles.push(circle);
+                circle.draw();
+            }
+
+            i++;
+        }
+    };
+
+    p.draw = function () {
+
+    };
+
+}, 'p5Container');
+
+class Circle {
+    constructor(p, pos, d) {
+        this.p = p;
+        this.pos = pos;
+        this.d = d;
+    }
+
+    draw() {
+        this.p.circle(this.pos.x, this.pos.y, this.d);
+    }
+
+    static overlaps(c1, c2) {
+        let diffVector = p5.Vector.sub(c1.pos, c2.pos);
+        let distance = c1.d / 2 + c2.d / 2;
+        return diffVector.mag() < distance;
+    }
+}
