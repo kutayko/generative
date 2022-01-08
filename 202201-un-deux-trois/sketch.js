9bfaaea5ee8adabde279d79a1cddef8e69d969b4
Kutay Kocayurt <kkocayurt@babbel.com>
2022-01-08 18:02:59 +0100

    vera molnar un deux trois

---
+++ 202201-un-deux-trois/sketch.js
@@ -0,0 +1,48 @@
+let sketch = new p5((p) => {
+
+    p.setup = function () {
+        p.createCanvas(600, 600);
+        p.background(255);
+        p.stroke(0);
+        p.strokeWeight(4);
+
+        p.angleMode(p.DEGREES);
+
+        let lineSizeX = 20;
+        let lineSizeY = 20;
+
+        let y = 0;
+        while (y < p.height) {
+            let x = 0;
+            while (x < p.width) {
+                p.push();
+
+                p.translate(x + lineSizeX / 2, y + lineSizeY / 2); // rotate from center
+                p.rotate(p.random(-90, 90));
+                p.translate(-lineSizeX / 2, -lineSizeY / 2); // draw from 0,0
+
+                if(y < p.height / 3) {
+                    p.line(lineSizeX / 2, 0, lineSizeX / 2, lineSizeY);
+                } else if (y < p.height / 3 * 2) {
+                    p.line(lineSizeX / 3, 0, lineSizeX / 3, lineSizeY);
+                    p.line(lineSizeX / 3 * 2, 0, lineSizeX / 3 * 2, lineSizeY);
+                } else {
+                    p.line(lineSizeX / 6, 0, lineSizeX / 6, lineSizeY);
+                    p.line(lineSizeX / 2, 0, lineSizeX / 2, lineSizeY);
+                    p.line(lineSizeX / 6 * 5, 0, lineSizeX / 6 * 5, lineSizeY);
+                }
+
+                p.pop();
+                x += lineSizeX;
+            }
+            y += lineSizeY;
+        }
+    };
+
+    p.draw = function () {
+
+    };
+
+}, 'p5Container');
+
+xxxxxx
