fb00f9432668c1640e8741274ae3c44a5c79e498
Kutay Kocayurt <kkocayurt@babbel.com>
2022-01-08 22:45:48 +0100

    update HTML colors

--- 202201-hypnotic-squares/sketch.js
+++ 202201-hypnotic-squares/sketch.js
@@ -1,12 +1,18 @@
 let sketch = new p5((p) => {
 
-    let x = 0;
-    let y = 0;
+    // Colors
+    let bgColor = [40, 50, 97];
+    let fgColor = [210, 29, 22];
 
     p.setup = function () {
         p.createCanvas(600, 600);
-        p.background(255);
-        p.stroke(0);
+
+        p.colorMode(p.HSL);
+        p.background(...bgColor);
+        p.stroke(...fgColor);
+        p.fill(...bgColor);
+
+        p.noLoop();
     };
 
     p.draw = function () {
