f3d9f0c7f047149021c234129b0313525625af60
Kutay Kocayurt <kkocayurt@babbel.com>
2022-01-12 17:46:40 +0100

    refactor to use oop

--- src/sketches/textures-watercolor.js
+++ src/sketches/textures-watercolor.js
@@ -1,96 +1,121 @@
 let sketch = new p5((p) => {
 
     // Colors
-    let bgColor = [40, 50, 97];
-    let fgColor = [11, 59, 51];
+    let COLORS = {
+        bg: [40, 50, 97],
+        fg: [11, 59, 51]
+    };
 
     p.setup = function () {
         p.createCanvas(600, 600);
 
         p.colorMode(p.HSL);
-        p.background(...bgColor);
+        p.background(...COLORS.bg);
 
         p.noLoop();
     };
 
     p.draw = function () {
-        let x = 300;
-        let y = 300;
-        let basePolygon = getBasePolygon(75, 10);
-
         let i = 0;
         while(i < 50) {
-            let polygon = expandPolygon(basePolygon);
-            drawPolygon(x, y, polygon, 0.04);
+            let polygon = new Polygon(75);
+            polygon.expand(3);
+            polygon.draw();
             i++;
         }
 
     };
 
-    let expandPolygon = (corners, depth=7) => {
-        if (depth <= 0) { return corners; }
-
-        let tmpVectors = [...corners];
-
-        let j = 0;
-        for (let i = 0; i < corners.length - 1; i++) {
-            let newCorner = getNewCorner(corners[i], corners[i + 1]);
-            tmpVectors.splice(j + i + 1, 0, newCorner);
-            j++;
+    class Corner {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+            this.varience = 0.5;
         }
 
-        // last point to first
-        let newCorner = getNewCorner(corners[corners.length - 1], corners[0]);
-        tmpVectors.splice(tmpVectors.length, 0, newCorner);
+        static createNew(c1, c2) {
+            let magMultiplier = p.random(0.2, 0.8);
+            let angle = p.randomGaussian(p.PI / 2, p.PI / 6);
 
-        return expandPolygon(tmpVectors, depth - 1);
+            let vDist = p5.Vector.dist(c1, c2);
+            let pointPos = p.randomGaussian(vDist / 2, vDist / 5);
+
+            let slope = p.atan2(c2.y - c1.y, c2.x - c1.x);
+            let midPoint = p.createVector(
+                c1.x + pointPos * p.cos(slope),
+                c1.y + pointPos * p.sin(slope),
+            );
+
+            let tmpVector = p5.Vector.sub(c1, c2);
+            tmpVector.mult(magMultiplier);
+            tmpVector.rotate(angle);
+
+            let newCorner = p5.Vector.add(midPoint, tmpVector);
+            return newCorner;
+        }
+
+        static splitAll(corners, depth=3) {
+            if (depth <= 0) { return corners; }
+
+            let tmpVectors = [...corners];
+
+            let j = 0;
+            for (let i = 0; i < corners.length - 1; i++) {
+                let newCorner = Corner.createNew(corners[i], corners[i + 1]);
+                tmpVectors.splice(j + i + 1, 0, newCorner);
+                j++;
+            }
+
+            // last point to first
+            let newCorner = Corner.createNew(corners[corners.length - 1], corners[0]);
+            tmpVectors.splice(tmpVectors.length, 0, newCorner);
+
+            return Corner.splitAll(tmpVectors, depth - 1);
+        }
     }
 
-    let getBasePolygon = (r, corners) => {
-        let vectors = [];
-        for (let i = 0; i < p.TWO_PI; i += p.TWO_PI / corners) {
-            let x = r * p.cos(i);
-            let y = r * p.sin(i);
-            vectors.push(p.createVector(x, y));
+    class Polygon {
+        constructor(size, x = p.width / 2, y = p.height / 2, edges = 10) {
+            this.size = size;
+            this.x = x;
+            this.y = y;
+            this.edges = edges;
+
+            this.opacity = 0.04;
+
+            this.corners = this.getBaseCorners();
         }
-        return vectors;
-    };
 
-    let getNewCorner = (v1, v2) => {
-        let magMultiplier = p.random(0.2, 0.8);
-        let angle = p.randomGaussian(p.PI / 2, p.PI / 6);
+        getBaseCorners() {
+            let corners = [];
+            for (let i = 0; i < p.TWO_PI; i += p.TWO_PI / this.edges) {
+                let x = this.size * p.cos(i);
+                let y = this.size * p.sin(i);
+                corners.push(p.createVector(x, y));
+            }
+            return corners;
+        }
 
-        let vDist = p5.Vector.dist(v1, v2);
-        let pointPos = p.randomGaussian(vDist / 2, vDist / 5);
+        expand(depth=3) {
+            this.corners = Corner.splitAll(this.corners, depth);
+        }
 
-        let slope = p.atan2(v2.y - v1.y, v2.x - v1.x);
-        let midPoint = p.createVector(
-            v1.x + pointPos * p.cos(slope),
-            v1.y + pointPos * p.sin(slope),
-        );
+        draw() {
+            p.noStroke();
+            p.fill(...COLORS.fg, this.opacity);
 
-        let tmpVector = p5.Vector.sub(v1, v2);
-        tmpVector.mult(magMultiplier);
-        tmpVector.rotate(angle);
+            p.push();
+            p.translate(this.x, this.y);
 
-        let newCorner = p5.Vector.add(midPoint, tmpVector);
-        return newCorner;
-    };
+            p.beginShape();
+            this.corners.map(c => {
+                p.vertex(c.x, c.y);
+            });
+            p.endShape(p.CLOSE);
 
-    let drawPolygon = (x, y, vectors, opacity) => {
-        p.noStroke();
-        p.fill(...fgColor, opacity);
+            p.pop();
+        }
 
-        p.push();
-        p.translate(x, y);
-
-        p.beginShape();
-        vectors.map(v => {
-            p.vertex(v.x, v.y);
-        });
-        p.endShape(p.CLOSE);
-
-        p.pop();
-    };
+    }
 
 }, 'sketch-container');
